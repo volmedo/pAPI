e80481eeb6bdcad1c397cb67b4c5d98bf9f9bd2a
volmedo <vicente.olmedo@gmail.com>
2019-06-05 11:24:09 +0200

    Function comments are nice

--- cmd/server/limiter.go
+++ cmd/server/limiter.go
@@ -10,6 +10,11 @@ import (
 	"github.com/ulule/limiter/v3/drivers/store/memory"
 )
 
+// newRateLimitedHandler creates a new middleware based on ulule/limiter package that
+// limits the request rate that is sent to the specified handler.
+// The returned rate-limited handler will allow up to rps requests per second to
+// handler. When the rate exceeds the limit, a "429 Too Many Requests" response will be
+// sent back without invoking the wrapped handler.
 func newRateLimitedHandler(rps int64, handler http.Handler) (http.Handler, error) {
 	if rps <= 0 {
 		return nil, fmt.Errorf("rps cannot be negative (rps = %d)", rps)
